fbf136ba4c051fea217e7e723eae40cc262006eb
Brijesh Limbachiya <brijeshl.brainerhub@gmail.com>
2024-10-25 15:51:51 +0530

    Updated

--- index.js
+++ index.js
@@ -25,10 +25,9 @@ app.set("views", viewsDir);
 
 const staticDir = path.join(__dirname, "public");
 app.use(express.static(staticDir));
-app.use(express.json());
-
 app.use(cors({ origin: "*" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
 app.get("/", async function (req, res) {
   return res.send(
