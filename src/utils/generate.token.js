33317b79a675d598ff245ac91367fdecc8b85e0e
Brijesh Limbachiya <brijeshl.brainerhub@gmail.com>
2025-04-03 19:25:34 +0530

    Updated

--- src/utils/generate.token.js
+++ src/utils/generate.token.js
@@ -7,15 +7,13 @@ const generateTokens = async (email, userId, isAdmin, isAgent, isSeller, isBuyer
       throw new Error("Email and userId are required to generate tokens");
     }
 
-    const accessTokenExpiry = process.env.ACCESS_TOKEN_EXPIRY;
-    const refreshTokenExpiry = process.env.REFRESH_TOKEN_EXPIRY;
+    const accessTokenExpiry = parseInt(process.env.ACCESS_TOKEN_EXPIRY);  
+    const refreshTokenExpiry = parseInt(process.env.REFRESH_TOKEN_EXPIRY);
 
     const payload = { email, id: userId, isAdmin, isAgent, isSeller, isBuyer };
 
-    const [accessToken, refreshToken] = [
-      jwt.sign(payload, process.env.ACCESS_TOKEN_PRIVATE_KEY, { expiresIn: accessTokenExpiry }),
-      jwt.sign(payload, process.env.REFRESH_TOKEN_PRIVATE_KEY, { expiresIn: refreshTokenExpiry }),
-    ];
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_PRIVATE_KEY, { expiresIn: accessTokenExpiry });
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_PRIVATE_KEY, { expiresIn: refreshTokenExpiry });
 
     await UserToken.create({
       userId,
