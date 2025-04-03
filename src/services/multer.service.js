98ae8eb34336eec9e347e176e79a777668ec7f79
Brijesh Limbachiya <brijeshl.brainerhub@gmail.com>
2025-04-03 14:53:38 +0530

    Fix

--- src/services/multer.service.js
+++ src/services/multer.service.js
@@ -6,7 +6,7 @@ const storage = multer.diskStorage({
 });
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 50 * 1024 * 1024 },
+  limits: { fileSize: 500 * 1024 * 1024 },
 });
 
 module.exports = upload;
