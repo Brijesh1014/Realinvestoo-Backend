85be87edeab1c8f46ff99c38595ffc874981d18a
Brijesh Limbachiya <brijeshl.brainerhub@gmail.com>
2025-05-14 17:29:16 +0530

    fix

--- src/routes/subscriptionPlan.route.js
+++ src/routes/subscriptionPlan.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const subscriptionPlanController = require("../controllers/subscriptionPlan.controller");
 const auth = require("../middlewares/auth.middleware");
 
-router.post("/createSubscriptionPlan",  auth([ "isAgent"]), subscriptionPlanController.createSubscriptionPlan);
+router.post("/createSubscriptionPlan", auth([ "isAdmin"]), subscriptionPlanController.createSubscriptionPlan);
 router.get("/getAllSubscriptionPlans",  auth(["isSeller", "isAdmin", "isBuyer", "isAgent"]), subscriptionPlanController.getAllSubscriptionPlans);
 router.get("/getSubscriptionPlanById/:id",  auth(["isSeller", "isAdmin", "isBuyer", "isAgent"]), subscriptionPlanController.getSubscriptionPlanById);
 router.put("/updateSubscriptionPlan/:id",  auth(["isSeller", "isAdmin", "isBuyer", "isAgent"]), subscriptionPlanController.updateSubscriptionPlan);
